497f19a139bb7aade95ba58928fb15402c669979
Drew Proebstel <d.proebstel@gmail.com>
2023-04-25 15:46:41 -0700

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,70 +10,72 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_230_406_074_910) do
+ActiveRecord::Schema.define(version: 2023_04_06_074910) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'bids', force: :cascade do |t|
-    t.integer 'value'
-    t.integer 'card_id'
-    t.integer 'player_id'
-    t.index ['card_id'], name: 'index_bids_on_card_id'
-    t.index ['player_id'], name: 'index_bids_on_player_id'
+  create_table "bids", force: :cascade do |t|
+    t.integer "value"
+    t.integer "card_id"
+    t.integer "player_id"
+    t.index ["card_id"], name: "index_bids_on_card_id"
+    t.index ["player_id"], name: "index_bids_on_player_id"
   end
 
-  create_table 'cards', force: :cascade do |t|
-    t.string 'name'
-    t.string 'image'
-    t.integer 'game_id'
-    t.integer 'bid_id'
-    t.integer 'player_id'
-    t.index ['bid_id'], name: 'index_cards_on_bid_id'
-    t.index ['game_id'], name: 'index_cards_on_game_id'
-    t.index ['player_id'], name: 'index_cards_on_player_id'
+  create_table "cards", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.integer "game_id"
+    t.integer "bid_id"
+    t.integer "player_id"
+    t.index ["bid_id"], name: "index_cards_on_bid_id"
+    t.index ["game_id"], name: "index_cards_on_game_id"
+    t.index ["player_id"], name: "index_cards_on_player_id"
   end
 
-  create_table 'currencies', force: :cascade do |t|
-    t.integer 'value', default: 300
-    t.integer 'player_id'
-    t.integer 'game_id'
-    t.index ['game_id'], name: 'index_currencies_on_game_id'
-    t.index ['player_id'], name: 'index_currencies_on_player_id'
+  create_table "currencies", force: :cascade do |t|
+    t.integer "value", default: 300
+    t.integer "player_id"
+    t.integer "game_id"
+    t.index ["game_id"], name: "index_currencies_on_game_id"
+    t.index ["player_id"], name: "index_currencies_on_player_id"
   end
 
-  create_table 'games', force: :cascade do |t|
-    t.string 'code'
-    t.integer 'cards_handled', default: 0
-    t.string 'player_one_uuid'
-    t.string 'player_two_uuid'
-    t.integer 'card_id'
-    t.integer 'player_id'
-    t.index ['card_id'], name: 'index_games_on_card_id'
-    t.index ['player_id'], name: 'index_games_on_player_id'
+  create_table "games", force: :cascade do |t|
+    t.string "code"
+    t.integer "cards_handled", default: 0
+    t.string "player_one_uuid"
+    t.string "player_two_uuid"
+    t.integer "card_id"
+    t.integer "player_id"
+    t.index ["card_id"], name: "index_games_on_card_id"
+    t.index ["player_id"], name: "index_games_on_player_id"
   end
 
-  create_table 'games_players', id: false, force: :cascade do |t|
-    t.bigint 'game_id', null: false
-    t.bigint 'player_id', null: false
-    t.index ['game_id'], name: 'index_games_players_on_game_id'
-    t.index ['player_id'], name: 'index_games_players_on_player_id'
+  create_table "games_players", id: false, force: :cascade do |t|
+    t.bigint "game_id", null: false
+    t.bigint "player_id", null: false
+    t.index ["game_id"], name: "index_games_players_on_game_id"
+    t.index ["player_id"], name: "index_games_players_on_player_id"
   end
 
-  create_table 'players', force: :cascade do |t|
-    t.string 'name'
-    t.string 'uuid'
-    t.integer 'game_id'
-    t.integer 'bid_id'
-    t.integer 'card_id'
-    t.index ['bid_id'], name: 'index_players_on_bid_id'
-    t.index ['card_id'], name: 'index_players_on_card_id'
-    t.index ['game_id'], name: 'index_players_on_game_id'
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.string "uuid"
+    t.integer "game_id"
+    t.integer "bid_id"
+    t.integer "card_id"
+    t.index ["bid_id"], name: "index_players_on_bid_id"
+    t.index ["card_id"], name: "index_players_on_card_id"
+    t.index ["game_id"], name: "index_players_on_game_id"
   end
 
-  create_table 'seed_cards', force: :cascade do |t|
-    t.string 'name'
-    t.string 'image'
-    t.string 'rarity'
-    t.string 'set'
+  create_table "seed_cards", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.string "rarity"
+    t.string "set"
   end
+
 end
