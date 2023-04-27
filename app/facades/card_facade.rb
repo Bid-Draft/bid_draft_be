b42131c9380f2882cda476b657e47648da85dbc4
Drew Proebstel <d.proebstel@gmail.com>
2023-04-26 20:55:58 -0700

    changes parameters of packs from tests to production and adds new sets to seeds

--- app/facades/card_facade.rb
+++ app/facades/card_facade.rb
@@ -1,27 +1,32 @@
 # frozen_string_literal: true
 
 class CardFacade
-  def self.get_cards(_set)
+  def self.get_cards(set)
     cards = []
     SeedCard
-
-    commons = SeedCard.where(set: 'RTR').where(rarity: 'common')
-    5.times do
-      seed = commons.order('RANDOM()').first
-      card = Card.create(name: seed.name, image: seed.image)
+    order = 1
+    6.times do
+      commons = SeedCard.where(set: set).where(rarity: 'common')
+        commons.order('RANDOM()')[0..10].each do |seed|
+        card = create_card(seed,order)
+        order += 1
+        cards << card
+        end
+      uncommons = SeedCard.where(set: set).where(rarity: 'uncommon')
+        uncommons.order('RANDOM()')[0..2].each do |seed|
+        card = create_card(seed,order)
+        order += 1
+        cards << card
+      end
+      seed = SeedCard.where(set: set).where(rarity: %w[rare mythic]).first
+      card = create_card(seed,order)
+      order += 1
       cards << card
     end
-    uncommons = SeedCard.where(set: 'RTR').where(rarity: 'uncommon')
-    3.times do
-      seed = uncommons.order('RANDOM()').first
-      card = Card.create(name: seed.name, image: seed.image)
-      cards << card
-    end
-    rares = SeedCard.where(set: 'RTR').where(rarity: %w[rare mythic])
-    seed = rares.order('RANDOM()').first
-    card = Card.create(name: seed.name, image: seed.image)
-    cards << card
-    cards.shuffle
     cards
+  end
+
+  def self.create_card(seed,order)
+    Card.create(name: seed.name, image: seed.image, order: order)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
-sets = ['RTR']
+sets = ['MOM', 'ONE', 'BRO', 'DMU', 'SNC', 'NEO', 'VOW', 'MID', 'AFR', 'STX', 'KHM',     
+'ZNR', 'IKO', 'M21', 'THB', 'ELD', 'M20', 'WAR', 'RNA', 'GRN', 'M19', 'DOM',     
+'RIX', 'XLN', 'HOU', 'AKH', 'AER', 'KLD', 'EMN', 'SOI', 'OGW', 'BFZ', 'ORI',     
+'DTK', 'FRF', 'KTK', 'M15', 'JOU', 'BNG', 'THS', 'M14', 'DGM', 'GTC', 'RTR',     
+'M13', 'AVR', 'DKA', 'ISD', 'M12']
+
 sets.each do |set|
   file = File.read("app/mtg_json/#{set}.json")
   data = JSON.parse(file)
