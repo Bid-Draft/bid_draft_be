310bd0ec9d0b061f87b9a68c32e6d5407fee5594
Drew Proebstel <d.proebstel@gmail.com>
2023-05-21 17:43:58 -0700

    add heroku site to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4000'
+    origins 'http://localhost:4000', 'https://vast-savannah-37442.herokuapp.com'
 
     resource '*',
              headers: :any,
